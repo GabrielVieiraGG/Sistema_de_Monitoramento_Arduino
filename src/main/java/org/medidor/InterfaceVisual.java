3913497d2e8a4e4a258264b40e0f6ef7f01fa9e1
GabrielVieiraGG <gabrielvieiragoncalvesgonzaga@gmail.com>
2026-04-07 20:24:35 -0300

    Ajustes na mensagem de saída.

--- src/main/java/org/medidor/InterfaceVisual.java
+++ src/main/java/org/medidor/InterfaceVisual.java
@@ -4,14 +4,14 @@ public class InterfaceVisual {
 
     public void exibirDados(DadosAmbiente dados) {
         System.out.println("\n[ TELA DO APLICATIVO - TEMPO REAL ]");
-        System.out.println("🌡️ Temperatura:  " + dados.getTemperatura() + " °C");
-        System.out.println("💧 Umidade:      " + dados.getUmidade() + " %");
-        System.out.println("☀️ Luminosidade: " + dados.getLuminosidade() + " %");
+        System.out.println("Temperatura:  " + dados.getTemperatura() + " °C");
+        System.out.println("Umidade:      " + dados.getUmidade() + " %");
+        System.out.println("Luminosidade: " + dados.getLuminosidade() + " %");
         System.out.println("---------------------------------------");
     }
 
     public void exibirAvisoVermelho(String mensagemDeAlerta) {
-        System.out.println("🚨 " + mensagemDeAlerta.toUpperCase());
+        System.out.println(mensagemDeAlerta.toUpperCase());
         System.out.println("---------------------------------------");
     }
 }
